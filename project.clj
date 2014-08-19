296dc18d5e3dfe895309df2f8bbfc68850d93000
Justin Lambert <jlambert@au.westfield.com>
2014-08-19 17:10:49 +1000

    parsing and encoding json, booyah

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [clj-http "1.0.0"]
+                 [cheshire "5.3.1"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler clojure-rest.handler/app}
   :profiles
--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -1,11 +1,17 @@
 (ns clojure-rest.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clj-http.client :as client]
+            [cheshire.core :refer :all]))
+
+(defn input [] (client/get "http://search-service.production.dbg.westfield.com/api/search/master/search.json?centre=sydney&term=co"))
+(defn json [] (parse-string (:body (input))))
+(defn result [] (json))
 
 (defroutes app-routes
 	(GET "/" []
-    (str "COUNTEM"))
+    (generate-string (result)))
   (GET "/user/:id" [id]
   	(str "Hello user " id))
   (route/not-found "Not Found"))
